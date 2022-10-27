67cefbfefcac28b926034d178da6f3d6291c6700
Dennis Wang <denniswang.self@gmail.com>
2022-10-26 18:37:37 -0700

    modularized child components, useState, useRef, handleAddTodo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList.js';
 
 function App() {
-  const [todo, setTodo] = useState([])
-  
+  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: true }, { id: 1, name: 'Todo 1', complete: false }])
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: 1, name: name, complete: false }];
+    })
+    todoNameRef.current.value = null;
+  }
+
   return (
     <>
-      <TodoList /> <br></br>
-      <input type="text" />
-      <button>Add Todo</button> <br></br>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Todo</button>
-      <button>Clear Complete</button>
+      <button>Clear Complete</button><br></br>
+      There are {todos.length} todos. <br></br><br></br>
+      <TodoList todos={todos} />
     </>
   )
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Todo from './Todo.js';
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
     <div>
-      Todo List
+      Todo List:
+      {todos.map(todo => {
+        return <Todo key={todo} todo={todo} />
+      })}
     </div>
   )
 }
